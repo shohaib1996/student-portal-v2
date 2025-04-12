a09ec17ea0931665a073e6a2be5541fe04d67f09
MHkhan994 <khanmahmud994@gmail.com>
2025-04-12 19:04:44 +0600

    calendar update

--- src/components/calendar/CreateEvent/EventForm.tsx
+++ src/components/calendar/CreateEvent/EventForm.tsx
@@ -258,7 +258,7 @@ const EventForm = ({ form, onSubmit, setCurrentDate, edit, event }: TProps) => {
                         <FormControl>
                             <div>
                                 <ColorPicker
-                                    className='h-10 bg-foreground'
+                                    className='h-10 bg-foreground border border-forground-border'
                                     value={field.value || ''}
                                     onChange={field.onChange}
                                 />
