b71f74471c1834fd3c294e8234c5f198886952eb
Md Ashraful Alam <62536223+mdshimul186@users.noreply.github.com>
2025-05-07 00:00:07 +0600

    paypal added

---
+++ src/app/(authenticated)/payment-history/_components/provider/PaypalButton.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { instance } from '@/lib/axios/axiosInstance';
+
+// Renders errors or successfull transactions on the screen.
+function Message({ content }: any) {
+    return <p>{content}</p>;
+}
+
+interface PaypalButtonProps {
+    trxId: string;
+    paypalConfig: any;
+    onSuccess: () => void;
+    onError: () => void;
+}
+
+function PaypalButton({
+    trxId,
+    paypalConfig,
+    onSuccess,
+    onError,
+}: PaypalButtonProps) {
+    const initialOptions = {
+        'client-id': paypalConfig.clientId,
+        'enable-funding': 'venmo',
+        'disable-funding': '',
+        'buyer-country': 'US',
+        currency: 'USD',
+        'data-page-type': 'product-details',
+        components: 'buttons',
+        'data-sdk-integration-source': 'developer-studio',
+    };
+
+    const [message, setMessage] = useState('');
+
+    return (
+        <div className='App'>
+            <PayPalScriptProvider options={initialOptions as any}>
+                <PayPalButtons
+                    style={{
+                        shape: 'rect',
+                        layout: 'vertical',
+                        color: 'gold',
+                        label: 'paypal',
+                    }}
+                    createOrder={async () => {
+                        try {
+                            const response = await instance.post(
+                                '/transaction/paypal/ordercreate',
+                                {
+                                    trxId,
+                                },
+                            );
+                            const orderData = response.data?.jsonResponse;
+
+                            if (orderData.id) {
+                                return orderData.id;
+                            } else {
+                                const errorDetail = orderData?.details?.[0];
+                                const errorMessage = errorDetail
+                                    ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                                    : JSON.stringify(orderData);
+
+                                throw new Error(errorMessage);
+                            }
+                        } catch (error) {
+                            console.error(error);
+                            setMessage(
+                                `Could not initiate PayPal Checkout...${error}`,
+                            );
+                        }
+                    }}
+                    onApprove={async (data, actions) => {
+                        try {
+                            const response = await instance.post(
+                                `/transaction/paypal/ordercapture`,
+                                {
+                                    orderID: data.orderID,
+                                    trxId,
+                                },
+                            );
+
+                            const orderData = response.data?.jsonResponse;
+                            // Three cases to handle:
+                            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                            //   (2) Other non-recoverable errors -> Show a failure message
+                            //   (3) Successful transaction -> Show confirmation or thank you message
+
+                            const errorDetail = orderData?.details?.[0];
+
+                            if (errorDetail?.issue === 'INSTRUMENT_DECLINED') {
+                                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                                // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                                return actions.restart();
+                            } else if (errorDetail) {
+                                // (2) Other non-recoverable errors -> Show a failure message
+                                throw new Error(
+                                    `${errorDetail.description} (${orderData.debug_id})`,
+                                );
+                            } else {
+                                // (3) Successful transaction -> Show confirmation or thank you message
+                                // Or go to another URL:  actions.redirect('thank_you.html');
+                                const transaction =
+                                    orderData.purchase_units[0].payments
+                                        .captures[0];
+                                setMessage(
+                                    `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`,
+                                );
+                                console.log(
+                                    'Capture result',
+                                    orderData,
+                                    JSON.stringify(orderData, null, 2),
+                                );
+                                onSuccess();
+                            }
+                        } catch (error) {
+                            console.error(error);
+                            setMessage(
+                                `Sorry, your transaction could not be processed...${error}`,
+                            );
+                            onError();
+                        }
+                    }}
+                />
+            </PayPalScriptProvider>
+            <Message content={message} />
+        </div>
+    );
+}
+
+export default PaypalButton;
--- src/components/program/online-courses/PaymentModal.tsx
+++ src/components/program/online-courses/PaymentModal.tsx
@@ -10,6 +10,8 @@ import {
     ArrowLeft,
     Calendar1,
     CalendarIcon,
+    Square,
+    CreditCard,
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -34,16 +36,35 @@ import {
 } from '@/components/ui/popover';
 import { Calendar } from '@/components/ui/calendar';
 import dayjs from 'dayjs';
+import PaypalButton from '@/app/(authenticated)/payment-history/_components/provider/PaypalButton';
 
-type PaymentMethod = 'paypal' | 'bank' | 'card' | 'custom' | null;
+type PaymentMethod = {
+    id: string | number;
+    provider: 'paypal' | 'square' | 'card' | 'custom' | null;
+    name: string;
+    borderColor: string;
+    bgColor: string;
+    additionalData?: any;
+    instruction?: string;
+    svg: React.FC<{ isActive?: boolean }>;
+};
 
 interface PaymentModalProps {
     open: boolean;
     onOpenChange: (open: boolean) => void;
+    availableMethods: any[];
 }
 
-export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
-    const [selectedMethod, setSelectedMethod] = useState<PaymentMethod>(null);
+export function PaymentModal({
+    open,
+    onOpenChange,
+    availableMethods,
+}: PaymentModalProps) {
+    // console.log(availableMethods);
+
+    const [selectedMethod, setSelectedMethod] = useState<PaymentMethod | null>(
+        null,
+    );
     const [amount, setAmount] = useState('');
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [uploadFiles, setUploadFiles] = useState<string[]>([]);
@@ -51,37 +72,61 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
     const [note, setNote] = useState('');
     const [current, setCurrent] = useState<Date | undefined>(new Date());
     const [addPayment, { isLoading }] = useAddPaymentApiMutation();
+    const [currentScreen, setCurrentScreen] = useState('payment');
+    const [createdTrx, setCreatedTrx] = useState<any>('');
 
-    const paymentMethods = [
-        {
-            id: 'paypal',
-            name: 'PayPal',
+    // const paymentMethods = [
+    //     {
+    //         id: 'paypal',
+    //         name: 'PayPal',
+    //         borderColor: 'border-primary',
+    //         bgColor: 'bg-[#e6ebfa]',
+    //         svg: Paypal,
+    //     },
+    //     {
+    //         id: 'bank',
+    //         name: 'Bank Transfer',
+    //         borderColor: 'border-primary',
+    //         bgColor: 'bg-[#e6ebfa]',
+    //         svg: BankTransfer,
+    //     },
+    //     {
+    //         id: 'card',
+    //         name: 'Debit or Credit Card',
+    //         borderColor: 'border-primary',
+    //         bgColor: 'bg-[#e6ebfa]',
+    //         svg: Card,
+    //     },
+    //     {
+    //         id: 'custom',
+    //         name: 'Custom',
+    //         borderColor: 'border-primary',
+    //         bgColor: 'bg-[#e6ebfa]',
+    //         svg: Custom,
+    //     },
+    // ];
+
+    const paymentMethods = availableMethods.map((method, i) => {
+        return {
+            id: i,
+            provider: method.provider,
+            name: method.name,
             borderColor: 'border-primary',
             bgColor: 'bg-[#e6ebfa]',
-            svg: Paypal,
-        },
-        {
-            id: 'bank',
-            name: 'Bank Transfer',
-            borderColor: 'border-primary',
-            bgColor: 'bg-[#e6ebfa]',
-            svg: BankTransfer,
-        },
-        {
-            id: 'card',
-            name: 'Debit or Credit Card',
-            borderColor: 'border-primary',
-            bgColor: 'bg-[#e6ebfa]',
-            svg: Card,
-        },
-        {
-            id: 'custom',
-            name: 'Custom',
-            borderColor: 'border-primary',
-            bgColor: 'bg-[#e6ebfa]',
-            svg: Custom,
-        },
-    ];
+            additionalData: method.additionalData,
+            instruction: method.instruction,
+            svg:
+                method?.provider === 'paypal'
+                    ? Paypal
+                    : method?.provider === 'square'
+                      ? Square
+                      : method?.name === 'bank'
+                        ? BankTransfer
+                        : method?.provider === 'card'
+                          ? Card
+                          : CreditCard,
+        };
+    });
 
     const handleMethodSelect = (method: PaymentMethod) => {
         setSelectedMethod(method);
@@ -124,7 +169,7 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
     const submitPayment = () => {
         if (selectedMethod) {
             const data = {
-                method: selectedMethod,
+                method: selectedMethod.provider,
                 amount: Number(amount),
                 attachment: uploadFiles[0],
                 note: note,
@@ -133,12 +178,18 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
             addPayment({ payload: data })
                 .unwrap()
                 .then((res) => {
-                    onOpenChange(false);
-                    toast.success('Payment added successfully!');
-                    setSelectedMethod(null);
-                    setAmount('');
-                    setUploadFiles([]);
-                    setNote('');
+                    console.log(res);
+                    setCreatedTrx(res?.transaction);
+                    if (selectedMethod?.provider === 'paypal') {
+                        setCurrentScreen('paypal');
+                    } else {
+                        onOpenChange(false);
+                        toast.success('Payment added successfully!');
+                        setSelectedMethod(null);
+                        setAmount('');
+                        setUploadFiles([]);
+                        setNote('');
+                    }
                 })
                 .catch((err) => {
                     console.error(err);
@@ -152,6 +203,8 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
         }
     };
 
+    console.log('selectedMethod', selectedMethod);
+
     // Content that was previously in DialogContent
     const modalContent = (
         <div className='flex flex-col h-full py-3'>
@@ -164,49 +217,80 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
                             Method <span className='text-[#f34141]'>*</span>
                         </label>
                         <div className='grid grid-cols-2 gap-2'>
-                            {paymentMethods.map(
-                                ({
-                                    id,
-                                    borderColor,
-                                    bgColor,
-                                    svg: SvgComponent,
-                                    name,
-                                }) => (
-                                    <div
-                                        key={id}
-                                        className={cn(
-                                            'border rounded-md p-4 flex flex-col items-center justify-center gap-2 cursor-pointer hover:border-primary transition-colors bg-foreground',
-                                            selectedMethod === id &&
-                                                `${borderColor} ${bgColor}`,
-                                        )}
-                                        onClick={() =>
-                                            handleMethodSelect(
-                                                id as PaymentMethod,
-                                            )
-                                        }
-                                    >
-                                        <div className='w-10 h-10 flex items-center justify-center'>
-                                            <SvgComponent
-                                                isActive={selectedMethod === id}
-                                            />
-                                        </div>
-                                        <span
-                                            className={cn(
-                                                'text-sm',
-                                                selectedMethod === id &&
-                                                    'text-primary font-semibold',
-                                            )}
-                                        >
-                                            {name}
-                                        </span>
+                            {paymentMethods.map((method) => (
+                                <div
+                                    key={method?.id}
+                                    className={cn(
+                                        'border rounded-md p-4 flex flex-col items-center justify-center gap-2 cursor-pointer hover:border-primary transition-colors bg-foreground',
+                                        selectedMethod?.provider ===
+                                            method.provider &&
+                                            `${method.borderColor} ${method.bgColor}`,
+                                    )}
+                                    onClick={() =>
+                                        handleMethodSelect(
+                                            method as PaymentMethod,
+                                        )
+                                    }
+                                >
+                                    <div className='w-10 h-10 flex items-center justify-center'>
+                                        <method.svg
+                                            isActive={
+                                                selectedMethod?.provider ===
+                                                method.provider
+                                            }
+                                        />
                                     </div>
-                                ),
-                            )}
+                                    <span
+                                        className={cn(
+                                            'text-sm',
+                                            selectedMethod ===
+                                                method.provider &&
+                                                'text-primary font-semibold',
+                                        )}
+                                    >
+                                        {method.name}
+                                    </span>
+                                </div>
+                            ))}
                         </div>
                     </div>
 
                     {/* Bank Transfer Details */}
-                    {selectedMethod === 'bank' && (
+                    {selectedMethod?.provider === 'custom' &&
+                        selectedMethod?.additionalData && (
+                            <div className='space-y-4 border rounded-md p-4 bg-foreground'>
+                                <h3 className='font-medium'>
+                                    {selectedMethod?.name} Information
+                                </h3>
+                                <div className='grid grid-cols-2 gap-4'>
+                                    {selectedMethod?.additionalData.map(
+                                        (data: any, index: number) => (
+                                            <div key={index}>
+                                                <label className='block text-sm mb-1'>
+                                                    {data.key}:
+                                                </label>
+                                                <Input
+                                                    defaultValue={data.value}
+                                                />
+                                            </div>
+                                        ),
+                                    )}
+                                </div>
+                            </div>
+                        )}
+
+                    {selectedMethod?.provider === 'custom' &&
+                        selectedMethod?.instruction && (
+                            <div className='space-y-4 border rounded-md p-4 bg-foreground'>
+                                <h3 className='font-medium'>
+                                    {selectedMethod?.name} Instruction
+                                </h3>
+                                <div className='text-sm text-muted-foreground'>
+                                    {selectedMethod?.instruction}
+                                </div>
+                            </div>
+                        )}
+                    {/* {selectedMethod === 'bank' && (
                         <div className='space-y-4 border rounded-md p-4 bg-foreground'>
                             <h3 className='font-medium'>
                                 Bank Transfer Information
@@ -250,7 +334,7 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
                                 </div>
                             </div>
                         </div>
-                    )}
+                    )} */}
 
                     {/* Amount */}
                     <div>
@@ -265,6 +349,7 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
                                 className='pl-6 text-dark-gray bg-foreground focus:border focus:border-border-primary-light focus:ring-0'
                                 placeholder='0.00'
                                 value={amount}
+                                type='number'
                                 onChange={(e) => setAmount(e.target.value)}
                             />
                         </div>
@@ -390,14 +475,41 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
             subTitle='Fill out the form to add new payment'
             customFooter={
                 <div className='p-5 bg-background'>
-                    <Button
-                        className='w-full bg-primary hover:bg-primary/90'
-                        onClick={submitPayment}
-                        disabled={!selectedMethod || !amount || isLoading}
-                    >
-                        Continue to Payment{' '}
-                        <ArrowRight className='ml-2 h-4 w-4' />
-                    </Button>
+                    {selectedMethod?.provider === 'square' ? (
+                        <Button
+                            className='w-full bg-primary hover:bg-primary/90'
+                            onClick={() => {
+                                toast.error('Coming Soon');
+                                // onOpenChange(false);
+                                // setSelectedMethod(null);
+                                // setAmount('');
+                                // setUploadFiles([]);
+                                // setNote('');
+                            }}
+                            disabled={!selectedMethod || !amount || isLoading}
+                        >
+                            Coming Soon
+                            <ArrowRight className='ml-2 h-4 w-4' />
+                        </Button>
+                    ) : currentScreen === 'payment' ? (
+                        <Button
+                            className='w-full bg-primary hover:bg-primary/90'
+                            onClick={submitPayment}
+                            disabled={!selectedMethod || !amount || isLoading}
+                        >
+                            Continue to Payment{' '}
+                            <ArrowRight className='ml-2 h-4 w-4' />
+                        </Button>
+                    ) : (
+                        <Button
+                            className='w-full bg-primary hover:bg-primary/90'
+                            onClick={() => setCurrentScreen('payment')}
+                            disabled={!selectedMethod || !amount || isLoading}
+                        >
+                            <ArrowLeft className='mr-2 h-4 w-4' />
+                            Back to Payment{' '}
+                        </Button>
+                    )}
                 </div>
             }
             buttons={
@@ -421,7 +533,32 @@ export function PaymentModal({ open, onOpenChange }: PaymentModalProps) {
                 </div>
             }
         >
-            {modalContent}
+            {currentScreen === 'payment' ? (
+                modalContent
+            ) : (
+                <PaypalButton
+                    paypalConfig={availableMethods.find(
+                        (method) => method?.provider === 'paypal',
+                    )}
+                    trxId={createdTrx?._id as string}
+                    onSuccess={() => {
+                        setCurrentScreen('payment');
+                        onOpenChange(false);
+                        setSelectedMethod(null);
+                        setAmount('');
+                        setUploadFiles([]);
+                        setNote('');
+                    }}
+                    onError={() => {
+                        setCurrentScreen('payment');
+                        onOpenChange(false);
+                        setSelectedMethod(null);
+                        setAmount('');
+                        setUploadFiles([]);
+                        setNote('');
+                    }}
+                />
+            )}
         </GlobalModal>
     );
 }
